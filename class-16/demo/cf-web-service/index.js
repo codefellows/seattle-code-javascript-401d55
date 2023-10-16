c51bbb014f6df5b56e00c86ef53e04bad3974d86
JacobKnaack <jacobknck@gmail.com>
2023-10-16 09:54:37 -0700

    updates class 16 demo

--- class-16/demo/cf-web-service/index.js
+++ class-16/demo/cf-web-service/index.js
@@ -6,12 +6,46 @@ const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
+class Garage {
+  constructor() {
+    this.bikes = [];
+  }
+
+  add(bike) {
+    this.bikes.push(bike);
+  }
+}
+
+class Bike {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+}
+
 const app = express();
+const garage = new Garage();
 
 app.use(cors());
 
 app.get('/bikes', (req, res) => {
-  res.send('You got bikes!');
+  try {
+    res.status(200).json(bikes);
+  } catch(e) {
+    console.log('server error', e);
+    next(e);
+  }
+});
+
+app.post('/bikes', (req, res) => {
+  try {
+    const bike = new Bike(req.body);
+    garage.add(bike);
+    res.status(201).json(bike);
+  } catch(e) {
+    console.log('server error', e);
+    next(e);
+  }
 });
 
 app.listen(PORT, () => {
